eaa45a76198240c515495437fc6d17a134aba8fb
ulvifk <92516870+ulvifk@users.noreply.github.com>
2024-11-27 17:47:00 +0300

    run setup and migrate to GUROBI 12

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-import org.gradle.jvm.tasks.Jar
+
 plugins {
     id ("java")
     id ("application")
@@ -22,7 +22,7 @@ dependencies {
     testImplementation ("org.junit.jupiter:junit-jupiter-api:5.9.0")
     testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 
-    implementation("com.gurobi:gurobi:11.0.2")
+    implementation("com.gurobi:gurobi:12.0.0")
     implementation ("com.google.code.gson:gson:2.10")
     implementation ("org.apache.logging.log4j:log4j-api:2.22.1")
     implementation ("org.apache.logging.log4j:log4j-core:2.22.1")
