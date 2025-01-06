4598b00d6fdbd0d6cb96cafe4d58f918c91d84c6
ulvifk <92516870+ulvifk@users.noreply.github.com>
2025-01-06 12:54:04 +0300

    tidy up repository

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
-#rm -rf newOutput
-#./gradlew runGraspLoop -PyamlConfigPath=yamlConfigGrasp.yaml
-#./gradlew runBeeLoop -PyamlConfigPath=yamlConfigBee.yaml
+./gradlew runGraspLoop -PyamlConfigPath=yamlConfigGrasp.yaml
+./gradlew runBeeLoop -PyamlConfigPath=yamlConfigBee.yaml
 ./gradlew runMipLoop -PyamlConfigPath=yamlConfigDiscrete.yaml
-#./gradlew runMipLoop -PyamlConfigPath=yamlConfigContinuous.yaml
-#
+./gradlew runMipLoop -PyamlConfigPath=yamlConfigContinuous.yaml
