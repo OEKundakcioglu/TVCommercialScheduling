d53c3cf4dd18f207870a35889f9a496443c89455
Ulvi Findik <ulvi.fk99@gmail.com>
2025-11-26 14:06:35 +0300

    clean up repo

--- run.sh
+++
@@ -1,4 +0,0 @@
-./gradlew runGraspLoop -PyamlConfigPath=yamlConfigGrasp.yaml
-#./gradlew runMipLoop -PyamlConfigPath=yamlConfigDiscrete.yaml
-#./gradlew runMipLoop -PyamlConfigPath=yamlConfigContinuous.yaml
-#./gradlew runBeeLoop -PyamlConfigPath=yamlConfigBee.yaml
