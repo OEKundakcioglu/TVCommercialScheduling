6d85c2e61bf4f8f60c3d24d1d8ed1fff68a07656
Ulvi Findik <ulvi.fk99@gmail.com>
2025-12-13 19:40:35 +0300

    correct batch run

--- batch_run.sh
+++ batch_run.sh
@@ -1,10 +1,11 @@
-./run.sh 1
-./run.sh 2
-./run.sh 3
-./run.sh 4
-./run.sh 5
-./run.sh 6
-./run.sh 7
-./run.sh 8
-./run.sh 9
-./run.sh 10
+./run_cpu_full_longer.slurm 1
+./run_cpu_full_longer.slurm 2
+./run_cpu_full_longer.slurm 3
+./run_cpu_full_longer.slurm 4
+./run_cpu_full_longer.slurm 5
+./run_cpu_full_longer.slurm 6
+./run_cpu_full_longer.slurm 7
+./run_cpu_full_longer.slurm 8
+./run_cpu_full_longer.slurm 9
+./run_cpu_full_longer.slurm 10
+
