654e70e802a1b8324729228d4e25ce0cfc76ae4c
Ulvi Findik <ulvi.fk99@gmail.com>
2026-01-15 16:24:35 +0300

    new setup

---
+++ sbatch_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+for i in {1..10}
+do
+   sbatch run.slurm "grasp_single_thread_commands/run_commands_${i}.txt"
+done
+
+for i in {1..10}
+do
+   sbatch run.slurm "grasp_parallel_commands/run_commands_${i}.txt"
+done
+
+for i in {1..2}
+do
+   sbatch run.slurm "mip_discrete_commands/run_commands_${i}.txt"
+done
