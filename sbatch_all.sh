a4082d4fb05a7c692fb628c24060ca9c5326400a
Ulvi Findik <ulvi.fk99@gmail.com>
2026-01-17 14:35:21 +0300

    update

--- sbatch_all.sh
+++ sbatch_all.sh
@@ -2,10 +2,10 @@
 
 for i in {1..10}
 do
-   sbatch run.slurm "grasp_single_thread_commands/run_commands_${i}.txt"
+   sbatch run.slurm "grasp_parallel_commands/run_commands_${i}.txt"
 done
 
 for i in {1..10}
 do
-   sbatch run.slurm "grasp_parallel_commands/run_commands_${i}.txt"
+   sbatch run.slurm "grasp_single_thread_commands/run_commands_${i}.txt"
 done
