8bed6bf5719baa4e2780fb52204c8be961de4dca
Ulvi Findik <ulvi.fk99@gmail.com>
2026-01-28 10:21:49 +0300

    new setup

--- sbatch_all.sh
+++ sbatch_all.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-for i in {1..10}
-do
-   sbatch run.slurm "grasp_parallel_commands/run_commands_${i}.txt"
-done
+#for i in {1..10}
+#do
+#   sbatch run.slurm "grasp_parallel_commands/run_commands_${i}.txt"
+#done
+#
+#for i in {1..10}
+#do
+#   sbatch run.slurm "grasp_single_thread_commands/run_commands_${i}.txt"
+#done
 
 for i in {1..10}
 do
-   sbatch run.slurm "grasp_single_thread_commands/run_commands_${i}.txt"
+   sbatch run.slurm "bee_commands/run_commands_${i}.txt"
 done
