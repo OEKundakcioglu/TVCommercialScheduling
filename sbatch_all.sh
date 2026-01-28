4ec22db029168129806625dd8faad293b55f06a3
Ulvi Findik <ulvi.fk99@gmail.com>
2026-01-28 12:45:51 +0300

    results

--- sbatch_all.sh
+++ sbatch_all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#for i in {1..10}
-#do
-#   sbatch run.slurm "grasp_parallel_commands/run_commands_${i}.txt"
-#done
+for i in {1..10}
+do
+   sbatch run.slurm "grasp_parallel_commands/run_commands_${i}.txt"
+done
 
 for i in {1..10}
 do
